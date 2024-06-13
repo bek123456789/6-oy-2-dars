a439ac700f36e9cde482c1297a94213ff8fd63ea
bek123456789 <redmirating@gmail.com>
2024-06-13 12:00:16 +0500

    done

---
+++ main.js
@@ -0,0 +1,29 @@
+let wrapper = document.querySelector('.mySwiper')
+let first = document.querySelector('.first_card')
+let second = document.querySelector('.second_card')
+let third = document.querySelector('.third_card')
+let fourth = document.querySelector('.fourth_card')
+
+first.innerHTML = `
+<p class='number'>01</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://corporate.walmart.com/content/dam/corporate/images/global-responsibility/sustainability/planet/nature/businessfornature-banner.png" alt="">
+`
+
+second.innerHTML = `
+<p class='number'>02</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-souvenirpixels-417074.jpg&fm=jpg" alt="">
+`
+
+third.innerHTML = `
+<p class='number'>03</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/1200px-Altja_j%C3%B5gi_Lahemaal.jpg" alt="">
+`
+
+fourth.innerHTML = `
+<p class='number'>04</p>
+<p class='p-lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni culpa soluta, dolorum at illum deleniti sint doloribus iure provident nostrum!</p>
+<img class='photo' src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C214%2C3008%2C1579&wid=1200&hei=630&scl=2.506666666666667" alt="">
+`
